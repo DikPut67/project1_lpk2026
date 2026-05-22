8bb4cd2bcc773df5a905f55831a654dae29f267d
DikPut67 <dikaputra2910@gmail.com>
2026-05-22 14:28:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,26 @@
 import streamlit as st
 
 st.title("Project Kel.12")
-import streamlit as st
+import streamlit as kl
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
+kl.markdown("*Streamlit* is **really** ***cool***.")
+kl.markdown('''
     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
     :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
+kl.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
-
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
-
-import streamlit as st
+import streamlit as pa
 
 pages = {
     "Your account": [
-        st.Page("create_account.py", title="Create your account"),
-        st.Page("manage_account.py", title="Manage your account"),
+        pa.Page("create_account.py", title="Create your account"),
+        pa.Page("manage_account.py", title="Manage your account"),
     ],
     "Resources": [
-        st.Page("learn.py", title="Learn about us"),
-        st.Page("trial.py", title="Try it out"),
+        pa.Page("learn.py", title="Learn about us"),
+        pa.Page("trial.py", title="Try it out"),
     ],
 }
 
-pg = st.navigation(pages)
+pg = pa.navigation(pa)
 pg.run()
