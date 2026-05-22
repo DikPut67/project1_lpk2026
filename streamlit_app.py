9d4bee872cfb16a5e1ce4828d42f91cdbad31743
DikPut67 <dikaputra2910@gmail.com>
2026-05-22 14:22:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,19 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+
+import streamlit as st
+
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
