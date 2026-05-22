17de236a9ff24da2fe04c0252ab68ce2c71ba7c9
DikPut67 <dikaputra2910@gmail.com>
2026-05-22 14:30:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,15 @@
 import streamlit as st
 
-st.title("Project Kel.12")
-import streamlit as kl
-
-kl.markdown("*Streamlit* is **really** ***cool***.")
-kl.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-kl.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-import streamlit as pa
-
 pages = {
     "Your account": [
-        pa.Page("create_account.py", title="Create your account"),
-        pa.Page("manage_account.py", title="Manage your account"),
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
     ],
     "Resources": [
-        pa.Page("learn.py", title="Learn about us"),
-        pa.Page("trial.py", title="Try it out"),
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
     ],
 }
 
-pg = pa.navigation(pa)
+pg = st.navigation(pages)
 pg.run()
